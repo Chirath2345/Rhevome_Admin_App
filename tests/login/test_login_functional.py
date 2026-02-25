981a0b37995918b46983891401e665d6a96c7c6a
Chirath0627 <chamu0627@gmail.com>
2026-02-25 11:00:25 +0530

    update feb 25

--- tests/login/test_login_functional.py
+++ tests/login/test_login_functional.py
@@ -51,21 +51,21 @@ class TestAdminLoginFunctional:
     #    assert error_message != "", "Email validation message is not displayed!"
     #    page.wait_for_timeout(1000)
 
-    def test_tc_05_verify_login_with_empty_fields(self, page):
-        """
-        TC_05: Verify LogIn with Empty field
-        """
-        login_page = LoginPage(page)
-        login_page.navigate()
-        login_page.sign_in_btn.click()
-        email_error = login_page.get_email_validation_message()
-        page.wait_for_timeout(1000)
-        pass_error = login_page.get_password_validation_message()
-        page.wait_for_timeout(1000)
-        assert (
-            email_error != "" or pass_error != ""
-        ), "Validation message not shown for empty fields!"
-        page.wait_for_timeout(1000)
+    #def test_tc_05_verify_login_with_empty_fields(self, page):
+    #    """
+    #    TC_05: Verify LogIn with Empty field
+    #    """
+    #    login_page = LoginPage(page)
+    #    login_page.navigate()
+    #    login_page.sign_in_btn.click()
+    #    email_error = login_page.get_email_validation_message()
+    #    page.wait_for_timeout(1000)
+    #    pass_error = login_page.get_password_validation_message()
+    #    page.wait_for_timeout(1000)
+    #    assert (
+    #        email_error != "" or pass_error != ""
+    #    ), "Validation message not shown for empty fields!"
+    #    page.wait_for_timeout(1000)
 
     def test_tc_06_verify_remember_me_functionality(self, page):
         """
@@ -74,4 +74,4 @@ class TestAdminLoginFunctional:
         login_page = LoginPage(page)
         login_page.navigate()
         login_page.login_with_remember_me("chamika@ceydigital.com", "Qwer@#12345")
-        expect(page.locator("div").nth(3)).to_be_visible()
+        expect(page.locator("div").nth(3)).to_be_visible() 
